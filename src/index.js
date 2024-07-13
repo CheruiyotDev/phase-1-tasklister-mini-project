0ac1e4ed7b048eb08d2dc6e2a850ae25871fe499
Cheruiyot Evans <chersoftwaredev@gmail.com>
2024-07-13 18:50:50 +0300

    Task lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const ul = document.querySelector("#tasks")
+
+  
+  
+  document.querySelector("#submit").addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const input = document.querySelector("#new-task-description");
+    const taskText = input.value;
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    ul.appendChild(li);
+
+    input.value = '';
+  });
 });
